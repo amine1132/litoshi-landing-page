111319221ee678ce12a63d9ca02480c0f663cc89
Yassine Harrach <y.harrach@hotmail.fr>
2023-05-19 23:38:17 +0200

    Récupération data top 10 tokens

--- src/bandeaudefilant.js
+++ src/bandeaudefilant.js
@@ -2,32 +2,38 @@ import React, { useState, useEffect } from 'react';
 import Data from './data.json';
 import './bandeaudefilant.css'
 import Slider from "react-slick";
+import axios from 'axios';
 
 const DataDisplay = () => {
   const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setData(Data);
-        console.log(Data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
-  }, []);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await axios.get('https://brc-20.io/api_prices');
+          setData(response.data);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+  
+      fetchData();
+    }, []);
+  
+    // Tri et extraction du top 10 des objets avec les prix les plus élevés
+    const sortedData = data.sort((a, b) => b.prix - a.prix);
+    const top10Data = sortedData.slice(0, 10);
+    console.log(top10Data);
   
   return (
     <div className='defilante'>
       <ul className='slide'>
-        {data.map((item, index) => (
+        {top10Data.map((item, index) => (
           <li key={index}>
             <span className='titre'>{item.token}</span><span>${parseFloat(item.price).toFixed(2)}</span>${Math.floor(item.marketCap).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
           </li>
         ))}
-        {data.map((item, index) => (
+        {top10Data.map((item, index) => (
           <li key={index}>
             <span className='titre'>{item.token}</span><span>${parseFloat(item.price).toFixed(2)}</span>${Math.floor(item.marketCap).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
           </li>
