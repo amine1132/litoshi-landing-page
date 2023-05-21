8dafe752f5c52920dd22b0bd243a487a5935b996
Yassine Harrach <y.harrach@hotmail.fr>
2023-05-21 13:23:59 +0200

    recup data from brc20 api pour bandeau

--- src/bandeaudefilant.js
+++ src/bandeaudefilant.js
@@ -2,14 +2,23 @@ import React, { useState, useEffect } from 'react';
 import './bandeaudefilant.css'
 import axios from 'axios';
 
+const apiKey = 'd5zQSpuvj2JO3vFD';
+
 const DataDisplay = () => {
   const [data, setData] = useState([]);
+  const [price, setPrice] = useState(null);
   
     useEffect(() => {
       const fetchData = async () => {
         try {
-          const response = await axios.get('https://brc-20.io/api_prices');
-          setData(response.data);
+          const response = await axios.get('https://brc20api.bestinslot.xyz/v1/get_brc20_tickers_info/vol_24h/desc/0/1');
+          setData(response.data.items.slice(0, 10));
+          const change = await axios.get('https://api.coinbase.com/v2/prices/BTC-USD/spot', {
+            headers: {
+                'Authorization': `Bearer ${apiKey}`,
+            },
+            });
+          setPrice(change.data.data.amount);
         } catch (error) {
           console.error(error);
         }
@@ -18,26 +27,39 @@ const DataDisplay = () => {
       fetchData();
     }, []);
   
-    // Tri et extraction du top 10 des tokens avec les plus grosses valos
-    const sortedData = data.sort((a, b) => b.marketCap - a.marketCap);
-    const top10Data = sortedData.slice(0, 10);
-    console.log(top10Data);
-  
   return (
     <div className='defilante'>
       <ul className='slide'>
-        {top10Data.map((item, index) => (
+        {data.map((item, index) => (
           <li key={index}>
-            <span className='titre'>{item.token}</span><span>${parseFloat(item.price).toFixed(2)}</span>${Math.floor(item.marketCap).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-          </li>
-        ))}
-        {top10Data.map((item, index) => (
-          <li key={index}>
-            <span className='titre'>{item.token}</span><span>${parseFloat(item.price).toFixed(2)}</span>${Math.floor(item.marketCap).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            <span className='titre'>${item.tick}</span>
+            <ItemDetails tick={item.tick} marketcap={item.marketcap*Math.pow(10, -8)*price} />
+            ${Math.floor(item.marketcap*Math.pow(10, -8)*price).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+             ~~~~ {parseFloat(item.change_24h).toFixed(2)}%
           </li>
         ))}
       </ul>
     </div>
+  );
+};
+
+const ItemDetails = ({ tick, marketcap }) => {
+  const [price, setPrice] = useState(null);
+
+  useEffect(() => {
+    // Effectuer la requête pour obtenir les informations détaillées du token
+    axios.get(`https://brc20api.bestinslot.xyz/v1/get_brc20_ticker/${tick}`)
+      .then(response => {
+        const maxSupplyValue = parseInt(response.data.ticker[0].max_supply, 10);
+        setPrice(marketcap/maxSupplyValue);
+      })
+      .catch(error => {
+        console.error(`Erreur lors de la requête pour obtenir les informations du token ${tick}:`, error);
+      });
+  }, [tick]);
+
+  return (
+    <span>${parseFloat(price).toFixed(2)}</span>
   );
 };
 
